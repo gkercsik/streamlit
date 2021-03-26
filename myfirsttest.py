833c60a7ecc2ec852af192326943f429617d98e7
Julian (Gyula) Kercsik <gk4shares@gmail.com>
2021-03-26 18:40:46 +1100

    Add files via upload

---
+++ myfirsttest.py
@@ -0,0 +1,78 @@
+
+#YouTube video URL: 
+
+
+import streamlit as st
+from datetime import date
+
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+Start = "2015-01-05"
+Today = date.today().strftime("%Y-%m-%d")
+
+st.title("Stock Prediction App")
+
+#stocks = ("AAPL", "GOOG", "MSFT", "AMZN", "TSLA", "BTC-USD")
+
+stocks = (0)
+selected_stocks = stocks
+selected_stocks = st.text_input("Select TICKER for prediction", stocks)
+
+n_years = st.slider("Years of prediction:", 0 , 4)
+period = n_years * 365
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, Start, Today)
+    data.reset_index(inplace=True)
+    return data 
+
+data_load_state = st.text("Load data...")
+data = load_data(selected_stocks)
+data_load_state.text("Loading data...done!")
+
+st.subheader('Raw data')
+st.write(data.tail())
+  
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock opening price'))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock closing price'))
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_raw_data()
+
+# Forecasting
+df_train = data[['Date', 'Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+st.subheader('Forecast data')
+st.write(forecast.tail())
+
+st.write('Forecast data')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write('Forecast components')
+fig2 = m.plot_components(forecast)
+st.write(fig2)
+
+#That's it Folks
+
+
+
+
+
+
+
+
+
