1190b28deb7d292a7b6d77cf51bbe581b01b5f4a
Julian (Gyula) Kercsik <gk4shares@gmail.com>
2021-03-30 17:54:08 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from datetime import date
+
+import yfinance as yf
+#
+
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+model_training = st.beta_container()
+
+with header:
+	st.title('Welcome to my awesome Stock Prediction project')
+	st.text('In this project I look into the transaction of taxis in NYC ...')  
+
+
+with dataset:
+	st.header('NYC taxis dataset')
+	st.text('I found this dataset on blablabla.com, ...')
+Start = "2015-01-05"
+Today = date.today().strftime("%Y-%m-%d")
+
+stocks = ("btc-usd")
+selected_stocks = stocks
+selected_stocks = st.text_input("Select TICKER for prediction", stocks)
+
+n_years = st.slider("Years of prediction:", 0 , 4)
+period = n_years * 365
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, Start, Today)
+    data.reset_index(inplace=True)
+    return data
+
+
+	#taxi_data = pd.read_csv('taxi_data.csv')
+	#st.write(taxi_data.head())
+
+
+
+#with features:
+	#st.header('The features I created')
+
+
+
+with model_training:
+	#st.header('Time to train the model!')
+	st.text('Here you get to choose the hyperparameters of the model and see how the performance changes')
+
