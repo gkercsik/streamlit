61a7ac10195c58cd15d94c137e437f7dd56f5252
Julian (Gyula) Kercsik <gk4shares@gmail.com>
2021-03-30 19:11:26 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import streamlit as st
 from datetime import date
 
 import yfinance as yf
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
-from plotly import graph_objs as go
+
 
 header = st.beta_container()
 dataset = st.beta_container()
